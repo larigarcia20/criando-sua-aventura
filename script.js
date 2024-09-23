0f788f81718983060be2ad9767fa50a1f6a06950
larigarcia20 <00001110026341sp@al.educacao.sp.gov.br>
2024-09-23 09:48:27 -0300

    projeto final

---
+++ script.js
@@ -0,0 +1,13 @@
+const avança= document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button=> {
+    button.addEventListener('clik,function()'{
+        const atual= document.querySelector('.ativo';
+            cons proximoPasso='passo-'+ this.getattribuate('data-proximo');
+
+            atual.classLit.remove('ativo');
+            document.getElementById(proximopasso).classList.add('ativo';
+
+    })
+        
+})
